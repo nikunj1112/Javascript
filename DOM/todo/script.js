b64ffd87d4d9f0d44f41a64cf1610ecd0d1cb713
RANA NIIKUNJ <8891nikunjrana@gmail.com>
2025-06-12 23:55:25 +0530

    Update script.js

--- DOM/todo/script.js
+++ DOM/todo/script.js
@@ -10,42 +10,64 @@ let todo = []
 
 function DisplayTodo() {
 
-    OutputBox.innerHTML = " ";
-
+    OutputBox.innerHTML = "  ";
 
     todo.forEach((todos, index) => {
         const div = document.createElement("div");
         const ul = document.createElement("ul");
         const li = document.createElement("li");
-        const button = document.createElement("button");
+        const btndiv = document.createElement("div")
+        const editButton = document.createElement("button");
+        const delButton = document.createElement("button");
 
-        button.innerHTML = "✖"
+
+        delButton.innerHTML = "✖"
+        delButton.style.fontSize = "24px"
+        editButton.innerHTML = "✏️"
+        editButton.style.fontSize = "20px"
         li.textContent = todos;
 
+        delButton.addEventListener('click', () => {
+            deleteTodo(index);
+        });
 
+        editButton.addEventListener('click', () => {
+            editTodo(index);
+        });
 
+        btndiv.appendChild(editButton);
+        btndiv.appendChild(delButton);
 
         div.appendChild(ul);
         ul.appendChild(li);
-        li.appendChild(button);
+        li.appendChild(btndiv);
 
         OutputBox.appendChild(div);
 
-
-
-
     });
+}
+
+function deleteTodo(index) {
+    todo.splice(index, 1);
+    DisplayTodo();
+
+}
+
+function editTodo(index) {
+    input.value = todo[index];
+    todo.splice(index, 1);
 
 }
 
 btn.addEventListener("click", () => {
-
     var value = input.value
     todo.push(value);
+    input.value = ""; 
     DisplayTodo();
-
-
+    
 });
+
+
 DisplayTodo();
 
 
